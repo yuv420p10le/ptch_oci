04adf7ce0a5fdd2f08ebc40c926929a9f763c919
yuv420p10le <139547685+yuv420p10le@users.noreply.github.com>
2025-03-28 11:33:23 +0300

    Merged with upstream.

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,41 @@
 
 set -ouex pipefail
 
-### Install packages
+# Install ghostty, get rid of previous terminal
+dnf5 -y copr enable pgdev/ghostty
+dnf5 -y install ghostty
+dnf5 -y copr disable pgdev/ghostty
+dnf5 -y remove ptyxis
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Install apps I want
+dnf5 -y install zsh neovim python3-neovim mkvtoolnix mkvtoolnix-gui \
+    python3-vapoursynth vapoursynth-devel vapoursynth-libs vapoursynth-plugins \
+    vapoursynth-tools tldr
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# VDPAU for NVIDIA
+dnf5 -y install nvidia-vaapi-driver libva-utils vdpauinfo
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# pyenv/Python deps
+dnf5 -y group install "development-tools"
+dnf5 -y group install "c-development"
+dnf5 -y install zlib-devel bzip2 bzip2-devel readline-devel sqlite \
+    sqlite-devel openssl-devel xz xz-devel libffi-devel findutils tk-devel
 
-#### Example for enabling a System Unit File
+# Certain build tools I need..
+dnf5 -y install libstdc++-static
 
+# Remove apps I don't care about
+dnf5 -y remove waydroid # Maybe later?
+dnf5 -y remove ghostscript # I don't print on desktop, and I hate CUPS
+dnf5 -y remove kate-libs # KWrite/Kate
+dnf5 -y remove *akonadi* # Calendar/emails/contacts etc.. I use Thunderbird
+dnf5 -y remove *krfb* # VNC remove desktop - I prefer RDP
+dnf5 -y remove kwalletmanager5
+
+# Fonts! After install, get Windows 11 fonts from the Windows ISO and put them in $HOME/.local/share/fonts
+dnf5 -y install --allowerasing --skip-broken google-noto-*
+dnf5 -y install --allowerasing --skip-broken jetbrains-mono-*
+dnf5 -y install --allowerasing --skip-broken fira-code-fonts
+
+# This was in the upstream build.sh.. whatever?
 systemctl enable podman.socket

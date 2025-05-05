6ff7375ccbc24221ca800b81e85c1a4df232f355
yuv420p10le <139547685+yuv420p10le@users.noreply.github.com>
2025-05-05 19:24:10 +0300

    Added Vesktop RPM

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,6 +7,9 @@ dnf5 -y install zsh neovim python3-neovim mkvtoolnix mkvtoolnix-gui \
     python3-vapoursynth vapoursynth-devel vapoursynth-libs vapoursynth-plugins \
     vapoursynth-tools tldr thunderbird usbtop v4l-utils libunity
 
+# Vesktop rpm - Flatpak is problematic; AppImage has some weirdness with GPU acceleration
+dnf5 -y install https://vencord.dev/download/vesktop/amd64/rpm
+
 # pyenv/Python deps
 dnf5 -y group install "development-tools"
 dnf5 -y group install "c-development"

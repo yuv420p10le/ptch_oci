8b54e57840f604e828f3213e7a4fd798b0884656
yuv420p10le <139547685+yuv420p10le@users.noreply.github.com>
2025-06-28 23:29:13 +0300

    Aurora has working Thunderbird

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,14 +4,13 @@
 
 set -ouex pipefail
 
-# Some apps such as Thunderbird and Vesktop will trigger notifications
+# Some apps such as Vesktop will trigger notifications
 # The notifications cannot be clicked on the Flatpak version of the apps
 # Workarounds include using the native RPM version of the apps, or the AppImage (if exists)
-# For Thunderbird, I just use upstream so I can click the notifications
 # For Vesktop, the AppImage version would normally be fine, but if I use the AppImage
 # in conjution with libunity, then it'll actually display the notification count in the app icon
 # v4l-utils is needed for OBS Virtual Camera
-dnf5 -y install thunderbird v4l-utils libunity
+dnf5 -y install v4l-utils libunity
 
 # Fonts I like; get Windows 11 fonts from the Windows ISO and put them in $HOME/.local/share/fonts
 dnf5 -y install --allowerasing --skip-broken google-noto-*

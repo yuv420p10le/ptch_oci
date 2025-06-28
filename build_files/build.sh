91874c63a7bcd09acb11669a3a926796fc7e0d86
yuv420p10le <139547685+yuv420p10le@users.noreply.github.com>
2025-06-29 00:20:32 +0300

    Slim it down..

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# My opinionated aurora-dx image
+# My opinionated aurora-dx image - I only need native vkcapture
 
 set -ouex pipefail
-
-# Fonts I like; get Windows 11 fonts from the Windows ISO and put them in $HOME/.local/share/fonts
-dnf5 -y install --allowerasing --skip-broken google-noto-*
-dnf5 -y install --allowerasing --skip-broken jetbrains-mono-*
-dnf5 -y install --allowerasing --skip-broken fira-code-fonts
 
 # obs-gamecapture COPR to allow OBS to capture OpenGL games
 dnf5 -y copr enable bazzite-org/obs-vkcapture
 dnf5 -y install obs-vkcapture
 dnf5 -y copr disable bazzite-org/obs-vkcapture
-
-# This was in the upstream build.sh.. whatever?
-systemctl enable podman.socket
